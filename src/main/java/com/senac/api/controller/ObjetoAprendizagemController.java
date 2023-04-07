591f67a601a91669558baecfadb146eee6044b4f
carlos731 <chtsca2001@gmail.com>
2023-04-07 11:43:33 -0300

    método adicionar objeto aprendizagem no controller

--- src/main/java/com/senac/api/controller/ObjetoAprendizagemController.java
+++ src/main/java/com/senac/api/controller/ObjetoAprendizagemController.java
@@ -1,11 +1,17 @@
 package com.senac.api.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.senac.api.entity.ObjetoAprendizagem;
+import com.senac.api.request.ObjetoAprendizagemRequest;
+import com.senac.api.response.ObjetoAprendizagemResponse;
 import com.senac.api.service.ObjetoAprendizagemService;
 
 import jakarta.validation.Valid;
@@ -17,7 +23,12 @@ public class ObjetoAprendizagemController {
 	@Autowired
 	private ObjetoAprendizagemService objetoAprendizagemService;
 	
-	public @ResponseEntity<ObjetoAprendizagemResponse>  adicionar(@Valid @RequestBody ObjetoAprendizagemRequest objReq){
-		
+	@PostMapping
+	public ResponseEntity<ObjetoAprendizagemResponse>  adicionar(@Valid @RequestBody ObjetoAprendizagemRequest objReq){
+		ModelMapper mapper = new ModelMapper();
+		ObjetoAprendizagem obj = mapper.map(objReq, ObjetoAprendizagem.class);
+		obj = objetoAprendizagemService.adicionar(obj);
+		return ResponseEntity<>(mapper.map(obj, ObjetoAprendizagemResponse.class), HttpStatus.CREATED);
+		//return ResponseEntity<>(mapper.map(obj, ObjetoAprendizagemResponse.class), HttpStatus.CREATED);
 	}
 }
