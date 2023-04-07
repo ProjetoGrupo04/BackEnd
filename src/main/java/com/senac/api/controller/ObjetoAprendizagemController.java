e6775c8b6ab81e59f24bda0b43271765990eab24
carlos731 <chtsca2001@gmail.com>
2023-04-07 11:31:34 -0300

    Adicionando classes request e responses

--- src/main/java/com/senac/api/controller/ObjetoAprendizagemController.java
+++ src/main/java/com/senac/api/controller/ObjetoAprendizagemController.java
@@ -1,5 +1,23 @@
 package com.senac.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senac.api.entity.ObjetoAprendizagem;
+import com.senac.api.service.ObjetoAprendizagemService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/objetoAprendizagem")
 public class ObjetoAprendizagemController {
 
+	@Autowired
+	private ObjetoAprendizagemService objetoAprendizagemService;
+	
+	public @ResponseEntity<ObjetoAprendizagemResponse>  adicionar(@Valid @RequestBody ObjetoAprendizagemRequest objReq){
+		
+	}
 }
